2a67208fc467d41be070de7182f7dee779a68a63
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2022-07-06 14:54:11 -0300

    autenticacao

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from ast import Return
+from django.contrib import messages
+from django.contrib.messages import constants
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.models import User
+from django.contrib import auth
+import os
+from django.conf import settings
+from .utils import email_html
+from .models import Ativacao
+from hashlib import sha256
 
-# Create your views here.
+
+def cadastro(request):
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            return redirect('/')
+        return render(request, 'cadastro.html')
+
+def logar(request):
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            return redirect('/')
+        return render(request, 'logar.html')
+
+
+def ativar_conta(request, token):
+    token = get_object_or_404(Ativacao, token=token)
+    if token.ativo:
+        messages.add_message(request, constants.WARNING, 'Essa token já foi usado')
+        return redirect('/auth/logar')
+    user = User.objects.get(username=token.user.username)
+    user.is_active = True
+    user.save()
+    token.ativo = True
+    token.save()
+    messages.add_message(request, constants.SUCCESS, 'Conta ativa com sucesso')
+    return redirect('/auth/logar')
+
+
+
+def sair(request):
+    auth.logout(request)
+    return redirect('/auth/logar')
