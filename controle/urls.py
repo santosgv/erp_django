354f3b6bc715c5d97c4f12739ecdb636342b5bce
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2022-07-20 18:04:29 -0300

    insersao de produtos

--- controle/urls.py
+++ controle/urls.py
@@ -5,8 +5,11 @@ from django.urls import path
 urlpatterns = [
     path('categorias', views.categorias, name='categorias'),
     path('produtos', views.produtos, name='produtos'),
+    path('inserirProduto',views.inserirProduto, name='inserirProduto'),
+
     path('estoque', views.estoque, name='estoque'),
     path('inserirEstoque',views.inserirEstoque, name='inserirEstoque'),
+
     path('vendas', views.vendas, name='vendas'),
     path('pessoas', views.pessoas, name='pessoas'),
 ]
--- controle/views.py
+++ controle/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from .models import Estoque, Venda, Produto
+from .models import Estoque, Venda, Produto, Categoria
 
 
 def categorias(request):
@@ -9,8 +9,23 @@ def categorias(request):
 def produtos(request):
     if request.method == "GET":
         Produtos = Produto.objects.all()
-
     return render(request,'produtos.html',{'Produtos':Produtos})
+
+def inserirProduto(request):
+    if request.method == "GET":
+        categorias = Categoria.objects.all()
+        return render(request,'inserirProduto.html',{'categorias':categorias})
+    else:
+        Produtos = Produto.objects.all()
+        nome = request.POST.get('Nome')
+        preco = request.POST.get('PRECO')
+        categoria = request.POST.get('categoria')
+
+        produto = Produto(nome =nome,preco=preco,categoria=Categoria.objects.get(id=categoria)
+
+        )
+        produto.save()
+        return render(request,'produtos.html',{'Produtos':Produtos})
 
 def estoque(request):
     if request.method == "GET":
