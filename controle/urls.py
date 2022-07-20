53efc5cc8d54a542c1220f69795ff21e8530fe3b
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2022-07-20 15:44:18 -0300

    INSERIR ESTOQUE

--- controle/urls.py
+++ controle/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('categorias', views.categorias, name='categorias'),
     path('produtos', views.produtos, name='produtos'),
     path('estoque', views.estoque, name='estoque'),
+    path('inserirEstoque',views.inserirEstoque, name='inserirEstoque'),
     path('vendas', views.vendas, name='vendas'),
     path('fornecedores', views.fornecedores, name='fornecedores'),
     path('pessoas', views.pessoas, name='pessoas'),
--- controle/views.py
+++ controle/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Estoque, Venda, Produto
 
 
@@ -15,7 +15,21 @@ def estoque(request):
         estoques = Estoque.objects.all()
         return render(request,'estoque.html', {'estoques': estoques})
 
+def inserirEstoque(request):
+    if request.method == "GET":
+        produtos = Produto.objects.all()
+        return render(request,'inserirEstoque.html',{'produtos':produtos})
+    else:
+        produto = request.POST.get('produto')
+        produto_id =Produto.objects.get(id=produto)
+        qtd = request.POST.get('quantidade')
 
+        
+        estoque = Estoque(produto=Produto.objects.get(id=produto_id.id),
+        quantidade = qtd
+        )
+        estoque.save()
+        return redirect('/controle/estoque')
 
 def vendas(request):
     return render(request,'vendas.html')
